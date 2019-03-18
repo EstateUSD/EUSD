faa4c348e84f269631707f0202154f06eb65de9a
eusdstablecoin <eusdstablecoin@gmail.com>
2019-03-18 16:53:36 +0000

    Fix rinkeby migration

--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,11 @@
  *
  */
 
-// const HDWalletProvider = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const HDWalletProvider = require('truffle-hdwallet-provider');
+ const infuraKey = "fj4jll3k";
+
+ const fs = require('fs');
+ const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -61,9 +61,9 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
      rinkeby: {
-       provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/'),
-//       network_id: 3,       // Ropsten's id
-       gas: 8000000,        // Ropsten has a lower block limit than mainnet
+       provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/${infuraKey}'),
+       network_id: 4,       // rinkeby's id
+       gas: 7011989,
        confirmations: 2,    // # of confs to wait between deployments. (default: 0)
        timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
 //       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
